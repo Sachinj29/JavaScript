162a6ed544019f8b274e684803141770b787aa15
Sachinj29 <114915781+Sachinj29@users.noreply.github.com>
2025-01-10 15:02:03 +0000

    Container Config Done

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,41 @@
+let score = "33"
+//let mars = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+
+console.log(ValueInNumber);
+
+let islogin = 1
+let booleanlog =  Boolean(islogin)
+console.log(booleanlog);
+
+//Operations :
+
+console.log(4+5);
+console.log(10-6);
+console.log(4*5);
+console.log(20/4);
+console.log(5%6);
+
+let str1 =  "Sachin"
+let str2 = " Jadhav"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 1 + 3);
+console.log(1 + 4 + "5");
+console.log("1" + "1");
+
+console.log(5+6*2);
+
+let counter =100;
+counter++;
+console.log(counter);
+
