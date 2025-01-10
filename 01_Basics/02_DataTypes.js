f08a260b684284a8aada5feaf9cfea926e0f43b1
Sachinj29 <114915781+Sachinj29@users.noreply.github.com>
2025-01-10 12:13:47 +0000

    Container Config Done

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -1,1 +1,20 @@
-console.log("Hii")
+console.log("JavaScript")
+
+let Name = "Sachin"
+let Age = 21
+let isLoggedIn = false
+let State = null
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+//boolean -> true/false
+// null -> standalone value
+// undefined ->
+// Symbol -> unique
+
+//object -> 
+
+console.log(typeof "Sachin");
+console.log(typeof null);
+console.log(typeof undefined);
