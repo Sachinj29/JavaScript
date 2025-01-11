a2de9748d5072b509040cf591efa3c7a967f746e
Sachinj29 <114915781+Sachinj29@users.noreply.github.com>
2025-01-11 05:58:08 +0000

    Container Config Done

---
+++ 01_Basics/06_Nums_and_maths.js
@@ -0,0 +1,28 @@
+const Score = 100
+console.log(Score);
+
+const Balance = new Number(101);
+console.log(Balance);
+
+console.log(Balance.toString().length);
+console.log(Balance.toFixed(3));
+
+const Balance1 = 23.8965
+console.log(Balance1.toPrecision(3));
+
+const Balance2 = 123.8965
+console.log(Balance2.toPrecision(3));
+
+const Hundred = 1000000;
+console.log(Hundred.toLocaleString());
+
+//--------> MATHS <-----------//
+
+console.log(Math);
+console.log(Math.abs(-12));
+console.log(Math.round(6.85));
+console.log(Math.round(6.25));
+console.log(Math.sqrt(6));
+
+console.log(Math.max(6,2,9,5));
+console.log(Math.min(6,8,5,1));
