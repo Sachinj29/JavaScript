aeb15b4b241cf3f99b203cbcea19f32ed35a2307
Sachinj29 <114915781+Sachinj29@users.noreply.github.com>
2025-01-11 06:15:48 +0000

    Container Config Done

---
+++ 01_Basics/07_DatesInJS.js
@@ -0,0 +1,10 @@
+//------------>  Dates   <----------------//
+
+let date = new Date()
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+console.log(typeof date);
+
+let mydate = new Date(2024, 11, 18);
+console.log(mydate.toDateString());
