c88c4cb95f2d672b97582bfe70d1b032014d02e6
Sachinj29 <114915781+Sachinj29@users.noreply.github.com>
2025-01-14 06:06:21 +0000

    Container Config Done

---
+++ 03_basics/02_Scopes.js
@@ -0,0 +1,39 @@
+//let  , var  ,  const
+
+// let a =10;
+// const b =20;
+// var c =30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+var c = 300;     // Global Scpoe 
+
+//{ Scope }  "{}" it show the scope
+
+if(true){
+    let a =10;
+    const b =20;
+    var c =30;        // In case of "var" it print the local scope value .
+
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
+var a = 300;     // Global Scope 
+
+if(true){
+    let a =10;
+    // const b =20;
+    // var c =30;        
+
+}
+
+console.log(a);
+
+
