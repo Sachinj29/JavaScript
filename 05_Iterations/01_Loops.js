69a86a5bce7fe2e99c7eb0ae643fb4e7edcd5262
Sachinj29 <114915781+Sachinj29@users.noreply.github.com>
2025-01-14 17:26:30 +0000

    Container Config Done

---
+++ 05_Iterations/01_Loops.js
@@ -0,0 +1,25 @@
+
+// Loop  :------------>
+
+// for(let i=0; i<=10; i++){
+//   console.log(i);
+// }
+
+//----------------------------------->
+
+// for(let i=0; i<5; i++){
+//   const element = i;
+//   console.log(element);
+// }
+
+//-------------------------------------->
+
+for(let i=0; i<5; i++){
+  const element = i;
+  if(element==5){
+    console.log("5 is best number");
+  }
+  console.log(element);
+}
+
+//-------------------------->
